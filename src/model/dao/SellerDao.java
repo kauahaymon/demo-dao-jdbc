99c08c6ef044b27c3c665f7ce7e4b496cc8dd653
Kauã Haymon <kauahaymon@hotmail.com>
2024-06-10 17:49:55 -0400

    Seller to sellerDao

--- src/model/dao/SellerDao.java
+++ src/model/dao/SellerDao.java
@@ -1,8 +1,10 @@
 package model.dao;
 
+import model.entities.Seller;
+
 import java.util.List;
 
-public interface Seller {
+public interface SellerDao {
     void insert(Seller obj);
     void update(Seller obj);
     void deleteById(Integer id);
