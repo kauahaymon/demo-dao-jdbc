377fcc9047214e6b2eac309da661e1038d023bbc
Kauã Haymon <kauahaymon@hotmail.com>
2024-06-10 15:15:04 -0400

    hashCode and equals added

--- src/model/entities/Department.java
+++ src/model/entities/Department.java
@@ -1,6 +1,7 @@
 package model.entities;
 
 import java.io.Serializable;
+import java.util.Objects;
 
 public class Department implements Serializable {
     private Integer id;
@@ -25,6 +26,19 @@ public class Department implements Serializable {
 
     public void setName(String name) {
         this.name = name;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Department that = (Department) o;
+        return Objects.equals(id, that.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(id);
     }
 
     public String toString() {
