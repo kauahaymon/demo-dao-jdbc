3457b76e4bbebf129876caae0bd92ade061ef5e8
Kauã Haymon <kauahaymon@hotmail.com>
2024-06-12 15:31:37 -0400

    Insert implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -5,6 +5,7 @@ import model.dao.SellerDao;
 import model.entities.Department;
 import model.entities.Seller;
 
+import java.util.Date;
 import java.util.List;
 
 public class Program {
@@ -23,5 +24,17 @@ public class Program {
         System.out.println("\n*** TEST 3: seller listAll ***");
         list = sellerDao.listAll();
         list.forEach(System.out::println);
+
+        System.out.println("\n*** TEST 4: seller insert ***");
+        Seller newSeller = new Seller(
+                null,
+                "Greg",
+                "greg@gmail.com",
+                new Date(),
+                4000.0,
+                new Department(2, null)
+        );
+        sellerDao.insert(newSeller);
+        System.out.println("Inserted! New id = " + newSeller.getId());
     }
 }
